0f5558c3d22a1948f7fe069475dd5dc4c39aa933
Oliver Leung <oal22@cornell.edu>
2021-09-18 12:29:50 -0400

    Implement algorithm with uniform distribution

---
+++ as18.py
@@ -0,0 +1,75 @@
+from lattice import Lattice, LatticePoint
+import numpy as np
+
+def avg(p1, p2):
+    # Take the average of two lattice points, provided that they are in the same
+    # coset
+    coords = np.mean([p1.coords, p2.coords], axis=0)
+    pt = LatticePoint(p1.lattice, coords)
+    return pt
+
+def iter(points):
+    # Perform one iteration
+    cp_to_vec = {}
+
+    # Bin according to mod 2L
+    for point in points:
+        coords_str = (point.coords % 2).astype(str)
+        parity = ''.join(coords_str)
+
+        if parity in cp_to_vec:
+            cp_to_vec[parity].append(point)
+        else:
+            cp_to_vec[parity] = [point]
+
+    # Print elements in each bin
+    # for key, val in cp_to_vec.items():
+    #     print(key + ':')
+    #     for pt in val:
+    #         print(pt)
+
+    pairs = []
+    new_points = []
+
+    # Naively select pairs within each coset
+    for _, val in cp_to_vec.items():
+        while len(val) > 1:
+            p1 = val.pop(0)
+            p2 = val.pop(0)
+            pair = (p1, p2)
+            pairs.append(pair)
+
+        # optionally keep unpaired vectors
+        new_points += val
+
+    # Get the average of each pair
+    for p1, p2 in pairs:
+        # print(p1, p2)
+        new_pt = avg(p1, p2)
+        # print(new_pt)
+        new_points.append(new_pt)
+
+    return new_points
+
+# Initial parameters
+basis = np.array([[1, 0], [0, 1]])
+lattice = Lattice(basis)
+N = 100
+iters = 10
+
+### Full algorithm
+
+points = []
+print('Points:')
+# Generate uniform distribution of points with coordinates less than 1000
+for i in range(N):
+    point = lattice.generate()
+    print(point)
+    points.append(point)
+
+# Perform one iteration
+for i in range(iters):
+    new_points = iter(points)
+    print('New Points:')
+    for point in new_points:
+        print(point)
---
+++ lattice.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Lattice:
+    def __init__(self, basis):
+        self.basis = basis
+        self.dim = basis.shape[0]
+
+    def generate(self, max_digits=3):
+        coords = np.random.rand(self.dim)
+        coords = np.round(coords * 10**max_digits)
+        coords = coords.astype(int)
+
+        return LatticePoint(self, coords)
+
+class LatticePoint:
+    def __init__(self, lattice, coords):
+        self.lattice = lattice
+        self.coords = coords
+
+    def __str__(self):
+        return str(self.vec)
+
+    def __eq__(self, other):
+        return self.coords == other.coords
+
+    @property
+    def vec(self):
+        return self.lattice.basis @ self.coords
