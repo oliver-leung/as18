ff8501512de88b4d2025002c35eacd2302902378
Oliver Leung <oal22@cornell.edu>
2021-12-01 16:14:27 -0500

    Refactor methods for partial binning

--- as18/as18.py
+++ as18/as18.py
@@ -1,11 +1,11 @@
-from collections import defaultdict
 from time import time
-from typing import List, Dict
+from typing import List
 
 import numpy as np
 from matplotlib import pyplot as plt
 
 from lattice import Lattice, LatticePoint, QaryLattice
+from partial import partial
 from utility import shortest, bin_by_parity, visualize
 
 
@@ -27,82 +27,6 @@ def as18_iter(points: List[LatticePoint]) -> List[LatticePoint]:
         ]
 
     return new_points
-
-
-def partial_bin(points: List[LatticePoint], start: int, end: int, mod: int) -> Dict[str, List[LatticePoint]]:
-    cp_to_vec = defaultdict(list)  # Mapping from Coord Parities to vector lists
-
-    # Bin according to mod 2L
-    for point in points:
-        if start < end:
-            parity_coords = point.coords[start: end]
-        else:
-            parity_coords = point.coords.take(range(start, end + 4), mode='wrap')
-
-        coords_str = (parity_coords % mod).astype(int).astype(str)
-        parity = ''.join(coords_str)
-
-        cp_to_vec[parity].append(point)
-
-    return cp_to_vec
-
-
-def partial_iter(points: List[LatticePoint], start: int, end: int, mod: int) -> List[LatticePoint]:
-    """Perform one iteration of the averaging algorithm."""
-    cp_to_vec = partial_bin(points, start, end, mod)
-
-    # Naively select pairs within each coset
-    pairs = []
-    for _, val in cp_to_vec.items():
-        while len(val) > 1:
-            p1 = val.pop(0)
-            p2 = val.pop(0)
-            pairs.append((p1, p2))
-
-    # Get the average of each pair
-    new_points = []
-    for p1, p2 in pairs:
-        pts_sum = p1 + p2
-        pts_diff = p1 - p2
-        new_points += [pts_sum, pts_diff]
-
-    return new_points
-
-
-def partial(lattice: Lattice) -> LatticePoint:
-    iters = 4
-    ldim = lattice.dim
-    if ldim % iters != 0:
-        raise ValueError('Currently only works with dimensions divisible by 4.')
-
-    plt.ion()
-    k = (ldim * 3) / 4  # Number of coords in the sliding window
-    N = int(10 * iters * 2 ** k)  # Number of starting vectors
-
-    iters = (np.log(ldim) + 2 * np.log(10)) / (2 * k / ldim - 1)
-    iters = int(iters)
-    iters = iters - (iters % 4)
-
-    points = [lattice.sample_dgd() for _ in range(N)]
-    for i in range(iters):
-        visualize(points)
-        start = int((i * k) % ldim)
-        end = int((start + k) % ldim)
-        # print('Binning', start, 'to', end)
-        new_points = partial_iter(points, start, end, 2**(i + 1))
-
-        new_points_norms = [pt.norm ** 2 for pt in new_points]
-        new_points_norms_means = np.mean(new_points_norms)
-        print('Average of square norms:', new_points_norms_means)
-
-        # Stop iteration if we've zeroed out all of the vectors
-        if new_points_norms_means == 0:
-            break
-
-        points = new_points
-
-    points = [pt / 8 ** (iters / 4) for pt in points]
-    return shortest(points)
 
 
 def as18(lattice: Lattice, N=1000, max_iters=50) -> LatticePoint:
